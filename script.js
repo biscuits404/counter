10eaaf064f813f8d948addfbe454f74cf8d8030a
biscuits404 <260456019+biscuits404@users.noreply.github.com>
2026-05-27 23:36:36 +0530

    add theme toggle

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const toggleButton = document.getElementById("toggle")
+toggleButton.addEventListener("click",() =>
+{
+    document.body.classList.toggle("light-mode");
+})
+
 let x = 0;
 function increment() {
     document.getElementById("count").innerHTML = x++
